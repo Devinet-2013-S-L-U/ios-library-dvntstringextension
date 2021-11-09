42f373a66e49d2c45d9f5c1e3c50e54eba744865
Raúl Vidal Muiños <raul.vidal@devinet.es>
2021-11-09 12:55:10 +0100

    Changes

--- Sources/DVNTStringExtension/Classes/DVNTStringExtension.swift
+++ Sources/DVNTStringExtension/Classes/DVNTStringExtension.swift
@@ -10,19 +10,19 @@ import UIKit
 
 extension String
 {
-    public static func localize(_ key: String, comment: String? = "") -> String
+    public static func localize(_ key: String, comment: String = "") -> String
     {
         return NSLocalizedString(key, comment: comment)
     }
     
-    public static func localize(_ key: String, fromClass aClass: AnyClass, forResource resource: String, ofType type: String, withComment comment: String? = "") -> String
+    public static func localize(_ key: String, fromClass aClass: AnyClass, forResource resource: String, ofType type: String, withComment comment: String = "") -> String
     {
         let path = Bundle(for: aClass).path(forResource: resource, ofType: type)!
         let bundle = Bundle(path: path) ?? Bundle.main
         return NSLocalizedString(key, bundle: bundle, comment: comment)
     }
     
-    public static func localize(_ key: String, fromBundle bundle: Bundle, withComment comment: String? = "") -> String
+    public static func localize(_ key: String, fromBundle bundle: Bundle, withComment comment: String = "") -> String
     {
         return NSLocalizedString(key, bundle: bundle, comment: comment)
     }
